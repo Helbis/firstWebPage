af5c41355fa3045efcc3162c13686529cd284e4b
Filip Helbig <filiphelbig97@gmail.com>
2020-04-18 14:37:04 +0200

    Initial functioning commit\np5js implemented, fullscreen implemented

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+let canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(canvas);
+*/
+
+function setup(){
+	let cnv = createCanvas(window.innerWidth, window.innerHeight);
+	cnv.parent("screen");
+}
+
+
+function draw(){
+	background(0);
+
+}
