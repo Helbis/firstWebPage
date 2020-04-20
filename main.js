95cbc94fec8a4897500fd8293b647639350c8018
Filip Helbig <filiphelbig97@gmail.com>
2020-04-20 21:08:54 +0200

    Dropped Rivebot due to problems with promises

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 const pink = "#fa00cc";
 const grey = [51, 51, 51];
-
-const divID = "screen";
-const radius = 200;
-let cnv;
 const fps = 2;
+const divID = "screen";
+
+let cnv;
+let bot;
+let button; 
+let input; 
 
 
-//Rivescript
-let bot = new RiveScript();
-bot.loadFile([
-  "brain/shortcuts.rive",
-  "brain/main.rive"
-]).then(console.log("Loading successful")).catch(console.error);
+const chat = function(){
+	console.log(input.value());
+}
+
 
 function setup(){
 	cnv = createCanvas(400, 400);
 	cnv.parent(divID);
 	frameRate(fps);
+
+	bot = new Bot();
+	button = select("#send");
+	input = select("#input");
+	
+	button.mousePressed(chat);
+
 }
 
 
@@ -26,5 +33,5 @@ function draw(){
 	cnv.style("width: 100%; height: 100%;");	
 	cnv.style("padding: 0; margin: 0; border: 0;");
 	
-
+	
 }
